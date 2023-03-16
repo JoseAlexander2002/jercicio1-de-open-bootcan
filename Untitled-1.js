7aaad4e385e30cf6b20de1bbed41c148beeceb1d
JoseAlexander2002 <124645229+JoseAlexander2002@users.noreply.github.com>
2023-03-16 14:49:43 -0600

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,13 @@
+let lista = new Array("Alexander",19,true)
+console.log(lista);
+
+const fecha_cadena = new Date ("July 15 2003")
+console.log(fecha_cadena)
+
+const libro = {
+  titulo: "El quijote de la mancha",
+  autor: "Miguel de cervantes savedra",
+  fecha: "1604"
+} 
+
+console.log(libro)
